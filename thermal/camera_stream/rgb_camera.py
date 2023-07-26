ee4100b5a59c45ec5739d9dc94f9f269f42f88dc
adio ihsan <adioihsan@gmail.com>
2023-07-26 18:51:37 +0700

    Initial project structure

---
+++ thermal/camera_stream/rgb_camera.py
@@ -0,0 +1,9 @@
+class rgb_camera:
+    def __init__(self):
+        print("checking camera")
+    
+    def start_stream() :
+        return "frame"
+    
+    def stop_stream():
+        return "stop"
