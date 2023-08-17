8bf4d9eda871f3383603cb43b2e5ad2f955fc3d7
adio ihsan <adioihsan@gmail.com>
2023-08-17 20:23:27 +0700

    remove unused file

--- config.py
+++
@@ -1,2 +0,0 @@
-SHOW_VIEW = True
-FACE_DETECTOR = "dlib_hog" # options :  dlib_cnn , dlib_hog
--- testmut.py
+++
@@ -1,7 +0,0 @@
-from multiprocessing import Process
-
-def pt1():
-    while True:
-        print("am spongebob")
-
-Process(target=pt1).start()
