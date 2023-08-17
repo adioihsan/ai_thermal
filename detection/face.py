3bec5f029efdd5e57072a54fb8363b1ed938652a
adio ihsan <adioihsan@gmail.com>
2023-08-17 20:26:01 +0700

    add depface and configuration

--- detection/face.py
+++ detection/face.py
@@ -1,14 +1,14 @@
 import cv2
-import dlib
 import time
+import configparser
+import dlib
+from deepface import DeepFace
 from imutils import face_utils,resize
 from queue import Queue
-from deepface import DeepFace
 
 try:
     import detection.utils as utils
     import device.Rgb_cam as cam
-    from config import *
 except ImportError:        
     import os
     import sys
@@ -17,48 +17,64 @@ except ImportError:
     sys.path.append(parent)
     import utils
     import device.Rgb_cam as cam
-    from config import *
 
-# detect face using dlib hog+svm linear
+# parse config file
+config = configparser.ConfigParser()
+config.read("config.ini")
+config_d = config["FaceDetection"]
+FACE_DETECTOR = config_d["detector"]
+SHOW_LANDMARK = config_d.getboolean("landmark")
+print(SHOW_LANDMARK)
+
 if FACE_DETECTOR == "dlib_hog":
     face_detector = dlib.get_frontal_face_detector()
-def detect_face_hog(frame):
+elif FACE_DETECTOR == "dlib_cnn":
+    face_detector = dlib.cnn_face_detection_model_v1('mmod_human_face_detector.dat')
+
+def detect_face_dlib_hog(frame):
     gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     face_det = face_detector(gray_frame)
     face_boxes = [utils.convert_and_trim_bb(gray_frame, r) for r in face_det]
     return face_boxes
 
-# detect face using dlib cnn
-if FACE_DETECTOR == "dlib_cnn":
-    cnn_detector = dlib.cnn_face_detection_model_v1('mmod_human_face_detector.dat')
-def detect_face_cnn(frame):
-        rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        rgb_frame = resize(frame, width=600)
-        face_det = cnn_detector(rgb_frame)
-        face_boxes = [utils.convert_and_trim_bb(frame, r.rect) for r in face_det]
-        return face_boxes
+def detect_face_dlib_cnn(frame):
+    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    rgb_frame = resize(frame, width=600)
+    face_det = face_detector(rgb_frame)
+    face_boxes = [utils.convert_and_trim_bb(frame, r.rect) for r in face_det]
+    return face_boxes
+
+def detect_face_deepface_ssd(frame):
+    face_dets = DeepFace.extract_faces(frame,align=False,detector_backend="ssd",enforce_detection=False)
+    face_boxes = map(lambda detection:[i for i in detection['facial_area'].values()],face_dets)
+    return list(face_boxes)
 
 
 #detect face landmarks containt 68 points in x,y cordinate
 landmark_detector = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 def detect_landmark(frame,x,y,w,h):
-        gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        face_ROI = dlib.rectangle(x,y,(x+w),(y+h))
-        landmark = landmark_detector(gray_frame,face_ROI)
-        landmark = face_utils.shape_to_np(landmark)
-        return landmark
+    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    face_ROI = dlib.rectangle(x,y,(x+w),(y+h))
+    landmark = landmark_detector(gray_frame,face_ROI)
+    landmark = face_utils.shape_to_np(landmark)
+    return landmark
 
 # locate forhead based on face_landmarks point
-def detect_forhead(frame,landmarks,face_height):
-        xl1,yl1 = landmarks[20]
-        xl2,yl2 = landmarks[23]
-        forhead_ROI = utils.forhead_ROI_dynamic(xl1,yl1,xl2,yl2,face_height)
-        xf,yf,wf,hf = forhead_ROI
-        return forhead_ROI
+def detect_forhead(frame,face_box,landmark=None):
+    forhead_ROI = []
+    x,y,w,h = face_box
+    if landmark is None:
+        forhead_ROI = utils.forhead_ROI_static(x,y,w,h)
+    else:
+        xl1,yl1 = landmark[20]
+        xl2,yl2 = landmark[23]
+        forhead_ROI = utils.forhead_ROI_dynamic(xl1,yl1,xl2,yl2,h)
+    return forhead_ROI
 
 FACE_DETECTOR_DICT = {
-    "dlib_cnn": detect_face_cnn,
-    "dlib_hog": detect_face_hog
+    "dlib_cnn": detect_face_dlib_cnn,
+    "dlib_hog": detect_face_dlib_hog,
+    "deepface_ssd": detect_face_deepface_ssd
 }
 
 def run(q_frame,q_rois) :
@@ -70,18 +86,19 @@ def run(q_frame,q_rois) :
         landmark_points = []
         for (x, y, w, h) in face_boxes:
             # detect face landmark
-            landmark = detect_landmark(frame,x,y,w,h)
-            landmark_points.append(landmark)
-
-            # detect forhead 
-            forhead = detect_forhead(frame,landmark,h)
-            forhead_boxes.append(forhead)
+            if SHOW_LANDMARK:
+                landmark = detect_landmark(frame,x,y,w,h)
+                landmark_points.append(landmark)
+                forhead = detect_forhead(frame,[x,y,w,h],landmark)
+                forhead_boxes.append(forhead)
+            else:
+                forhead = detect_forhead(frame,[x,y,w,h])
+                forhead_boxes.append(forhead)
 
         # put detections result into queue
         if not q_rois.full():
             q_rois.put({"face":face_boxes,"forhead":forhead_boxes,"landmark":landmark_points})
-
-
+            
 if __name__ == "__main__":
     from multiprocessing import Process , Queue
     from threading import Thread
@@ -107,7 +124,7 @@ if __name__ == "__main__":
                 exit()
 
     def load_frame(q_frame):
-        camera = cam.Camera().start()
+        camera = cam.Camera(1).start()
         while True:
             success,frame = camera.get_frame()
             q_frame.put(frame)
